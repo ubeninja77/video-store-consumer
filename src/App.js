5be6f5a5543026141b9a10b91d013b899bc65497
Lola Llanes <mslourdesllanes@gmail.com>
2020-06-23 09:07:13 -0700

    finised adding router

--- src/App.js
+++ src/App.js
@@ -2,6 +2,16 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
+import Home from './components/Home';
+
+
 class App extends Component {
   render() {
     return (
