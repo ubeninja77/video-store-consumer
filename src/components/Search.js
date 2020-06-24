e3d2e8f726fd42704e5c5bb31b242cb61b2057e9
thenora <thenora@gmail.com>
2020-06-24 12:06:09 -0700

    trying search bar from web

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,4 +1,60 @@
 import React, { Component } from 'react';
 import axios from 'axios';
-import SearchBar from './SearchBar';
+import SearchBox from './SearchBox';
 
+class Search extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      foundMovies: [],
+    }
+  }
+
+  resetState = () => {
+    this.setState({
+      foundMovies: [],
+    });
+  }
+
+  searchMovies = (query) => {
+    if (query!== null) {
+      axios.get('http://localhost:4000/movies/', {
+        params: {
+          query: query,
+        }
+      })
+      .then((response) => {
+        if (response.data !== undefined) {
+          this.setState({
+            foundMovies: response.data
+          });
+        };
+      })  
+      .catch((error) => {
+        this.setState({
+          error: error.errors,
+          foundMovies: [],
+        });
+      })
+    }
+  }
+
+  // TODO add movie to library
+  
+  // TODO list movies
+
+  render() {
+    return (
+      <section>
+        <SearchBox 
+        submitSearchQueryCallback={this.searchMovies}
+        />
+      </section>
+    )
+  }
+
+
+};
+
+export default Search;
