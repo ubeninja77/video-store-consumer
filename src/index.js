0e154bdc1dc8f9b2cd048da7d428eb2e26cf8cf1
Lola Llanes <mslourdesllanes@gmail.com>
2020-06-24 18:20:47 -0700

    added css and boostrap import

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import "./index.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 ReactDOM.render(<App baseUrl="http://localhost:3000/" />, document.getElementById('root'));
 registerServiceWorker();
